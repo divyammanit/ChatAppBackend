3b5d373252e946a70e253aeff4206a5429cdcb29
divyammanit <105696827+divyammanit@users.noreply.github.com>
2023-02-11 14:23:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express=require('express');
 const cors=require('cors');
 const {addUser,removeUser,getUser,getUserInRoom} = require('./users');
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const router=require('./router');
 
 const app=express();
